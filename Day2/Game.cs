11c99a3f47ecc717afaad118881437c6a9a9c9d7
Emma Frisk <frisk.emma@hotmail.com>
2023-12-02 21:58:43 +0100

    day 2 part 1

---
+++ Day2/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Day2
+{
+    class Game
+    {
+        public List<int> Red { get; set; } = new();
+        public List<int> Blue { get; set; } = new();
+        public List<int> Green { get; set; } = new();
+
+        public Game() { }
+        public Game(string gameString) 
+        {
+            foreach (var cubes in gameString.Split(';'))
+            {
+                foreach (var round in cubes.Split(','))
+                {
+                    if (round.Contains("blue"))
+                    {
+                        Blue.Add(int.Parse(round.Trim().Substring(0, round.Trim().IndexOf(' ')+1)));
+                    }
+                    else if (round.Contains("red"))
+                    {
+                        Red.Add(int.Parse(round.Trim().Substring(0, round.Trim().IndexOf(' ')+1)));
+                    }
+                    else if (round.Contains("green"))
+                    {
+                        Green.Add(int.Parse(round.Trim().Substring(0, round.Trim().IndexOf(' ')+1)));
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2023");
+
+            Dictionary<int, Game> games = new Dictionary<int, Game>();
+            var fileStream = new FileStream(@"C:\Users\frisk\source\repos\AdventOfCode2023\Day2\puzzledata.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.Length > 0)
+                    {
+                        var id = int.Parse(line.Substring(5, line.IndexOf(':')-5));
+                        games.Add(id, new Game(line.Substring(line.IndexOf(':') + 1)));
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            var sumOfIdsOfPossibleGames = 0;
+            foreach (var game in games)
+            {
+                if (PossibleGame(game.Value))
+                {
+                    sumOfIdsOfPossibleGames += game.Key;
+                }
+            }
+            Console.WriteLine("Day 2, part 1: " + sumOfIdsOfPossibleGames);
+
+        }
+
+
+        // only 12 red cubes, 13 green cubes, and 14 blue cubes
+        private const int redCubes = 12;
+        private const int greenCubes = 13;
+        private const int blueCubes = 14;
+
+        private static bool PossibleGame(Game game)
+        {
+            if (game.Red.Max() > redCubes)
+            { 
+                return false;
+            }
+            if (game.Green.Max() > greenCubes)
+            { 
+                return false;
+            }
+            if (game.Blue.Max() > blueCubes)
+            { 
+                return false;
+            }
+            return true;
+        }
+    }
+}
